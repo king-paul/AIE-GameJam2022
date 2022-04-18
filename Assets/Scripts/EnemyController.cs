014752c28ca7bb39396b72d154f20c2467932a44
Paul King <PaulKing09@bigpond.com>
2022-04-19 00:55:05 +1000

    Created waypoint prefab and implement patrolling script

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,30 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum PlayerState
+public enum EnemyState
 {
-    standby,
-    patrol,
-    follow
+    Standby,
+    Patrol,
+    Follow
 }
 
 public class EnemyController : MonoBehaviour
 {
     NavMeshAgent agent;
     Transform player;
-    PlayerState state;
+    EnemyState state;
+
+    public EnemyState State { set => state = value; }
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        player = GameObject.FindWithTag("Player").transform;
-        
+        player = GameObject.FindWithTag("Player").transform;      
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
+        if(state == EnemyState.Follow)
+            agent.destination = player.position;
     }
 }
--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyPatrol : MonoBehaviour
 {
+    public Transform[] patrtolRoute;
+
+    private int wayPointNum = 0;
+    NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = patrtolRoute[wayPointNum].position;
+
+        GetComponent<EnemyController>().State = EnemyState.Patrol;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform == patrtolRoute[wayPointNum])
+        {
+            if (wayPointNum < patrtolRoute.Length - 1)
+                wayPointNum++;
+            else
+                wayPointNum = 0;
+
+            agent.destination = patrtolRoute[wayPointNum].position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
     }
 }
