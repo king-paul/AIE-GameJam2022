e632955e77c218891565d69f90473d5666f404e5
Paul King <PaulKing09@bigpond.com>
2022-04-18 22:37:53 +1000

    Created Player controller in sample scene with movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float moveSpeed = 1f;
+    public Vector3 cameraOffset = new Vector3(0, 12.8f, -8.92f);
+
+    CharacterController controller;
+    Transform camera;
+    Vector3 direction;
+    Vector3 vertical, horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        camera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = transform.forward * Input.GetAxis("Vertical");
+        horizontal = transform.right * Input.GetAxis("Horizontal");
+        direction = new Vector3(horizontal.x, 0, vertical.z);
+        
+        controller.Move(direction.normalized * moveSpeed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        camera.position = transform.position + cameraOffset;
+    }
+}
