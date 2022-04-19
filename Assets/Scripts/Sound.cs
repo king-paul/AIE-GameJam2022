6c7cb630b2e1114fc0aa226bff36b1abe7f7d259
Zuesue <85092888+Zuesue@users.noreply.github.com>
2022-04-19 18:18:48 +1000

    Readded 1x1 Prefab. Added improved audio manager

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,20 +1,23 @@
-using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [System.Serializable]
 public class Sound
 {
-
     public string name;
-    
+
     public AudioClip clip;
 
     [Range(0f, 1f)]
     public float volume;
-    [Range(.1f, 3f)]
+    [Range(.1f, 2f)]
     public float pitch;
 
-   [HideInInspector]
+    public bool loop;
+
+    [HideInInspector]
     public AudioSource source;
 
 }
