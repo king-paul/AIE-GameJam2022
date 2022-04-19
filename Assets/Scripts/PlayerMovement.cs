a85c0af25d259dd87282715c02057d05c2d697f2
Paul King <PaulKing09@bigpond.com>
2022-04-19 12:07:21 +1000

    Updated Player Movement script and player prefab
    - Player can now move either straight or diagonally depending on whether the diagonal movement is turned on
    - Camera offset can now be turned on or off
    - An orthographic camera has been attached to the player prefab as a child

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,69 +5,58 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Range(0, 10)]
-    public float moveSpeed = 1f;
-    public Vector3 cameraOffset = new Vector3(0, 12.8f, -8.92f);
+    public float moveSpeed = 3f;
+    public bool diagonalMovement = false;
+    public bool offSetCamera = false;
+    public Vector3 cameraOffset = new Vector3(0, 12.8f, -8.92f);    
 
     CharacterController controller;
     Transform camera;
     Vector3 direction;
     Vector3 vertical, horizontal;
+    Vector3 upRight, downRight;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        camera = Camera.main.transform;
+        camera = transform.GetChild(0);
+
+        if (camera.rotation.y >= 0)
+        {
+            upRight = new Vector3(1, 0, 1);
+            downRight = new Vector3(1, 0, -1);
+        }
+        else
+        {
+            upRight = new Vector3(-1, 0, 1);
+            downRight = new Vector3(1, 0, 1); 
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        if (diagonalMovement)
         {
             vertical = transform.forward * Input.GetAxis("Vertical");
-            horizontal = transform.right * Input.GetAxis("Vertical") * -1;
-
-            direction = new Vector3(horizontal.x, 0, vertical.z);
-
-            controller.Move(direction.normalized * moveSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("s"))
-        {
-            vertical = transform.forward * Input.GetAxis("Vertical");
-            horizontal = transform.right * Input.GetAxis("Vertical") * -1;
-
-            direction = new Vector3(horizontal.x, 0, vertical.z);
-
-            controller.Move(direction.normalized * moveSpeed * Time.deltaTime);
-
-        }
-        if (Input.GetKey("a"))
-        {
-            vertical = transform.forward * Input.GetAxis("Horizontal");
             horizontal = transform.right * Input.GetAxis("Horizontal");
-
             direction = new Vector3(horizontal.x, 0, vertical.z);
-
-            controller.Move(direction.normalized * moveSpeed * Time.deltaTime);
-
-
         }
-        if (Input.GetKey("d"))
+        else
         {
-            vertical = transform.forward * Input.GetAxis("Horizontal");
-            horizontal = transform.right * Input.GetAxis("Horizontal");
-
-            direction = new Vector3(horizontal.x, 0, vertical.z);
-
-            controller.Move(direction.normalized * moveSpeed * Time.deltaTime);
-
-
+            vertical = upRight * Input.GetAxis("Vertical");
+            horizontal = downRight * Input.GetAxis("Horizontal");
+            
+            direction = vertical + horizontal;
         }
+
+        controller.Move(direction.normalized * moveSpeed * Time.deltaTime);
     }
+
     private void LateUpdate()
     {
-        camera.position = transform.position + cameraOffset;
+        if(offSetCamera)
+            camera.position = transform.position + cameraOffset;
     }
 }
-
