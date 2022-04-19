4e7aa4f21654087d2e7f790bfa3ec9b4a7a2ab9e
Paul King <PaulKing09@bigpond.com>
2022-04-19 15:22:06 +1000

    Added enemy rotation while standing still
    Also got the first scene in the build index to load when colliding with enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,6 +7,7 @@ public enum EnemyState
 {
     Standby,
     Patrol,
+    Look,
     Follow
 }
 
@@ -14,7 +15,19 @@ public class EnemyController : MonoBehaviour
 {
     NavMeshAgent agent;
     Transform player;
-    EnemyState state;
+    [SerializeField] EnemyState state;
+
+    [Header("Look State")]
+    [Range(-180, 180)]
+    public float leftAngle = -90;
+    [Range(-180, 180)]
+    public float rightAngle = 90;
+    [Range(1, 100)]
+    public float turnSpeed = 30f;
+    [Range(0, 10)]
+    public float waitTime = 1f;
+
+    bool clockwise;
 
     public EnemyState State { set => state = value; }
 
@@ -22,7 +35,7 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        player = GameObject.FindWithTag("Player").transform;      
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -30,5 +43,50 @@ public class EnemyController : MonoBehaviour
     {
         if(state == EnemyState.Follow)
             agent.destination = player.position;
+
+        if (state == EnemyState.Look)
+            LookInRange();
+    }
+
+    void LookInRange()
+    {
+        float angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
+
+        //Debug.Log(angle);
+        
+        if(!clockwise)
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+
+            if (angle <= leftAngle)
+            {
+                //Debug.Log("anti-clockwise Angle has been reached");
+                clockwise = true;
+                State = EnemyState.Standby;
+
+                Invoke("StartLooking", waitTime);
+            }
+
+        }
+        else
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+
+            angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
+            if (angle >= rightAngle)
+            {
+                //Debug.Log("anti-clockwise Angle has been reached");
+                clockwise = false;
+                State = EnemyState.Standby;
+
+                Invoke("StartLooking", waitTime);
+            }
+        }
+
+    }
+
+    private void StartLooking()
+    {
+        State = EnemyState.Look;
     }
 }
